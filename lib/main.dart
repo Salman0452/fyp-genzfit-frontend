7e4f5f60c80e7b2b88e24a7f6c40740e0721227c
Salman Ahmad <sa26122612@gmail.com>
2025-11-15 21:23:09 +0500

    install dotnev library

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'core/theme/app_colors.dart';
 import 'rotes/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+  
   await Firebase.initializeApp(
     options: const FirebaseOptions(
         apiKey: 'AIzaSyDSBPfkd3Hx4Q8jsb0jgbkJEbR5LBHKQlk',
